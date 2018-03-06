4eb9a5834945e7c9104f0c32b7f738f6be5bfa99
Sean Takafuji <seanytak@hawaii.edu>
2018-03-06 00:55:10 -1000

    Added Bootstrap Integration

--- farms/views.py
+++ farms/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+
+def index(request):
+    response = "The farmer has id %s"
+    template = loader.get_template('farms/index.html')
+    return HttpResponse(template.render({}, request))
+
 
 # Create your views here.
